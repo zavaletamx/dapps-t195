ded307f09e7d2ce18db5540c9629251477c90eb5
ZavaletaMX <raul.zavaletazea@gmail.com>
2021-01-19 08:43:10 -0600

    Creación de íconos personalizados para la app, generación de screens adicionales y uso de LinearLayout

---
+++ app/src/main/java/mx/edu/uteq/dapps/prueba195/EjemploLinearActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.uteq.dapps.prueba195;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EjemploLinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo_linear);
+    }
+}
