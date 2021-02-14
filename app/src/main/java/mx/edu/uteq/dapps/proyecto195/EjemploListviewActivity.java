a003336a870cfc15bd29cabc99d9b44ddacf4db9
ZavaletaMX <raul.zavaletazea@gmail.com>
2021-02-14 17:49:54 -0600

    Manejo de eventos y Swipe sobre una lista

--- app/src/main/java/mx/edu/uteq/dapps/proyecto195/EjemploListviewActivity.java
+++ app/src/main/java/mx/edu/uteq/dapps/proyecto195/EjemploListviewActivity.java
@@ -1,11 +1,15 @@
 package mx.edu.uteq.dapps.proyecto195;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,17 +28,29 @@ public class EjemploListviewActivity extends AppCompatActivity {
      */
     private List<String> coleccion;
 
+    private AlertDialog.Builder alerta;
+
+    /*
+    SwipeRefresh para actualizar el contenido
+    del listview
+     */
+    private SwipeRefreshLayout srlLengProg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejemplo_listview);
 
+        srlLengProg = findViewById(R.id.srl_lenprog);
+
+        alerta = new AlertDialog.Builder(this);
+
         lvLenProg = findViewById(R.id.lv_lp);
         coleccion = new ArrayList<>();
 
         /* Agregar elementos a una lista */
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 1; i++) {
             coleccion.add("Java");
             coleccion.add("C++");
             coleccion.add("Python");
@@ -60,5 +76,89 @@ public class EjemploListviewActivity extends AppCompatActivity {
         );
 
         lvLenProg.setAdapter(adaptador);
-    }
+
+        /*
+        Click de un item dentro de un ListView
+         */
+        lvLenProg.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(EjemploListviewActivity.this, position + "", Toast.LENGTH_SHORT).show();
+                String elemSel = lvLenProg.getItemAtPosition(position).toString();
+
+                alerta.setTitle("¡Hey!")
+                .setMessage("Seleccionaste:\n"+position + " - " + elemSel)
+                .setIcon(R.drawable.info)
+                .setNeutralButton("Aceptar", null)
+                .setPositiveButton(null, null)
+                .setNegativeButton(null, null)
+                .setCancelable(false)
+                .show();
+
+            }
+        });
+
+        /*
+        Evento long click
+         */
+        lvLenProg.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                alerta.setTitle("¿Eliminar?")
+                        .setMessage("¿Realmente deseas eliminar este elemento?")
+                        .setIcon(R.drawable.trash)
+                        .setNeutralButton(null, null)
+                        .setNegativeButton("No", null)
+                        .setPositiveButton("Si, eliminar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //Metodo remove de un ArrayList indicando
+                                //la pos a eliminar
+                                coleccion.remove(position);
+
+                                //Notificamoa al adaptador
+                                //que hay un cambio en la coleccíon
+                                adaptador.notifyDataSetChanged();
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+                return false;
+            }
+        });
+
+        srlLengProg.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                /*
+                Acciones a realizar cuando se arrastre y suelte
+                la lista
+                 */
+
+                //Eliminamos los elementos de la colección
+                coleccion.clear();
+
+                //Agregamos a la lista, los lenguaje
+                //originales
+                coleccion.add("Java");
+                coleccion.add("C++");
+                coleccion.add("Python");
+                coleccion.add("Objective-C");
+                coleccion.add("Swift");
+                coleccion.add("React");
+                coleccion.add("JavaScript");
+                coleccion.add("Dart");
+                coleccion.add("Flutter");
+                coleccion.add("PHP");
+
+                //Notificar al adaptador
+                adaptador.notifyDataSetChanged();
+
+
+                //ocultar loader
+                srlLengProg.setRefreshing(false);
+            }
+        });
+
+    }// /ONCREATE
 }
